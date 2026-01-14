38f1c6cadeb3b915f498c2305141088cbc142da4
ThanhSanglt02 <th.sang1210@gmail.com>
2026-01-14 23:35:24 +0700

    first test commit

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -19,7 +19,11 @@ export class BasePage {
         return this.page.locator(`//*[contains(text(), "${text}")]`);
     }
 
-    getdynamicElementContainsText(text: string) {
+    dynamicElementContainsText1(text: string) {
+        return this.page.locator(`//*[contains(text(), "${text}")]`);
+    }
+
+    dynamicElementContainsText2(text: string) {
         return this.page.locator(`//*[contains(text(), "${text}")]`);
     }
 }
